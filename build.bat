82c0e06cf902fec385fe79097ffa3649e932816d
David Sferruzza <david.sferruzza@gmail.com>
2015-10-15 12:55:25 +0200

    Change slides engine

--- build.bat
+++
@@ -1,2 +0,0 @@
-pandoc -t dzslides -s --css=styles.css talk-programmation-fonctionnelle-en-js.md -o talk-programmation-fonctionnelle-en-js.html
-pandoc -t dzslides -s --self-contained --css=styles.css talk-programmation-fonctionnelle-en-js.md -o packed.html
